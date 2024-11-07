a05aa1c681ab751c9170a7abc53d664ff95f6bd1
zayola <angel.zayola@gmail.com>
2024-11-07 00:00:40 -0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,7 +13,7 @@ st.write('The current movie title is ', title)
 
 #session = get_active_session()
 cnx = st.connection("snowflake")
-session = cnx.session
+session = cnx.session()
 
 
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
